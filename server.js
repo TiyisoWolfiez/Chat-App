41b3d32f6de047ede40e479c40b97b6ea26d8a24
tiyiso <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-06-30 21:27:00 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,4 +6,16 @@ const server = require("http").createServer(app);
 
 app.use(express.static(path.join(__dirname+"/public")));
 
+io.on("connection", function(socket) {
+    socket.on("newuser", function(username){
+        socket.broadcast.emit("update", username + " joined the conversation.");
+    });
+    socket.on("exituser", function(username){
+        socket.broadcast.emit("update", username + " left the conversation.");
+    });
+    socket.on("chat", function(message){
+        socket.broadcast.emit("chat", message);
+    });
+});
+
 server.listen(5000);
